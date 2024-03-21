7aa429ecf8b24fbf2a2879829c669956bfb3e93a
Anton Knysh <anton@profairgames.com>
2024-03-21 11:35:37 +0200

    reworked

--- src/customHooks/useWindowResize.jsx
+++ src/customHooks/useWindowResize.jsx
@@ -7,7 +7,7 @@ export default function useWindowResize() {
     height: 0,
   });
 
-  const handleResize = () => {
+  const hadleWindowResize = () => {
     setWindowSize({
       width: window.innerWidth,
       height: window.innerHeight,
@@ -15,14 +15,40 @@ export default function useWindowResize() {
   };
 
   useLayoutEffect(() => {
-    handleResize();
+    hadleWindowResize();
 
-    window.addEventListener("resize", handleResize);
+    window.addEventListener("resize", hadleWindowResize);
 
     return () => {
-      window.removeEventListener("resize", handleResize);
+      window.removeEventListener("resize", hadleWindowResize);
     };
   }, []);
 
   return windowSize;
 }
+
+// export default function useWindowResize() {
+//   const [windowSize, setWindowSize] = useState({
+//     width: 0,
+//     height: 0,
+//   });
+
+//   const handleResize = () => {
+//     setWindowSize({
+//       width: window.innerWidth,
+//       height: window.innerHeight,
+//     });
+//   };
+
+//   useLayoutEffect(() => {
+//     handleResize();
+
+//     window.addEventListener("resize", handleResize);
+
+//     return () => {
+//       window.removeEventListener("resize", handleResize);
+//     };
+//   }, []);
+
+//   return windowSize;
+// }
